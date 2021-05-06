33af660276f9f0a1ac0276c69dbf867fac6a2613
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2021-05-06 00:16:59 -0300

    Update Vol_ativos.py

--- Vol_ativos.py
+++ Vol_ativos.py
@@ -4,12 +4,36 @@ import matplotlib.pyplot as plt
 ########### Importando os dados
 
 df = pd.read_excel('economatica.xlsx', parse_dates=True, index_col=0).dropna()
-
+print(df.columns)
 #### Calculando IBOV
 ibov = pd.DataFrame()
 
 for year in ['2006', '2010', '2014', '2018']:
     preço_ibov = df.loc[year, ['IBOV']].reset_index(drop=True)
     preço_ibov.rename(columns={'IBOV':year}, inplace=True)
-    ibov = pd.concat([ibov, preço_ibov], axis=1)
+    ibov = pd.concat([ibov, preço_ibov], axis=1).dropna()
+
+#### Calculando IMAB 5
+imab = pd.DataFrame()
+
+for year in ['2006', '2010', '2014', '2018']:
+    preço_imab = df.loc[year, ['IMAB 5']].reset_index(drop=True)
+    preço_imab.rename(columns={'IMAB 5':year}, inplace=True)
+    imab = pd.concat([imab, preço_imab], axis=1).dropna()
+
+#### Calculando IMAB 5+
+imab_5 = pd.DataFrame()
+
+for year in ['2006', '2010', '2014', '2018']:
+    preço_imab_5 = df.loc[year, ['IMAB 5+']].reset_index(drop=True)
+    preço_imab_5.rename(columns={'IMAB 5+':year}, inplace=True)
+    imab_5 = pd.concat([imab_5, preço_imab_5], axis=1).dropna()
+
+#### Calculando Dólar
+dol = pd.DataFrame()
+
+for year in ['2006', '2010', '2014', '2018']:
+    preço_dol = df.loc[year, ['DÓLAR']].reset_index(drop=True)
+    preço_dol.rename(columns={'DÓLAR':year}, inplace=True)
+    dol = pd.concat([dol, preço_dol], axis=1).dropna()
 
